816c6c7ac005080c6a9ba5d4d20afd486a224a0f
Armela Ligori <armela.ligori@nebula.al>
2023-01-04 14:32:08 +0100

    Documents Section fix

--- src/components/Landing/DocumentsSection.js
+++ src/components/Landing/DocumentsSection.js
@@ -3,58 +3,52 @@ import Badge from '../../core/badges/Badge';
 import { FileCheck } from 'tabler-icons-react';
 import details from '../../assets/images/application-details.png';
 
+const ListItem = ({ title, text, icon, bgColor }) => {
+  return (
+    <div className="flex items-start my-6">
+      <div className={`${bgColor} text-white p-3 rounded-md mr-5`}>{icon}</div>
+      <div className="flex-1 flex-col">
+        <h3 className="font-medium text-base">{title}</h3>
+        <p className="text-sm text-gray-500 my-1">{text}</p>
+      </div>
+    </div>
+  );
+};
+
 const DocumentsSection = () => {
   return (
-    <div className="bg-white shadow flex flex-col lg:flex-row items-center pb-10">
+    <div className="bg-white shadow flex flex-col lg:flex-row items-start pb-10">
       <div className="flex-1">
         <img src={details} alt="" className="shadow" />
       </div>
       <div className="flex-1 px-10 lg:px-20">
-        <Badge text="Client Application" />
+        <Badge className="my-1" text="Client Application" />
         <h3 className="font-bold text-2xl mt-1">Application Documents in one place</h3>
         <p className="text-sm text-gray-500 mt-2 mb-8">
           Lorem ipsum dolor sit amet consectetur adipiscing elit. Pariatur minima sequi recusandae,
           porro maiores officia.
         </p>
-
-        <div className="flex items-start my-5">
-          <div className="bg-red-400 text-white p-3 rounded-md mr-5">
-            <FileCheck />
-          </div>
-          <div className="flex-1 flex-col">
-            <h3 className="font-medium text-base">Fill it digitally</h3>
-            <p className="text-sm text-gray-500 my-1">
-              Lorem ipsum dolor sit amet consectetur adipiscing elit. Pariatur minima sequi
-              recusandae, porro maiores officia.
-            </p>
-          </div>
-        </div>
-
-        <div className="flex items-start my-5">
-          <div className="bg-blue-400 text-white p-3 rounded-md mr-5">
-            <FileCheck />
-          </div>
-          <div className="flex-1 flex-col">
-            <h3 className="font-medium text-base">Save it for other cases</h3>
-            <p className="text-sm text-gray-500 my-1">
-              Lorem ipsum dolor sit amet consectetur adipiscing elit. Pariatur minima sequi
-              recusandae, porro maiores officia.
-            </p>
-          </div>
-        </div>
-
-        <div className="flex items-start my-5">
-          <div className="bg-[#256D85] text-white p-3 rounded-md mr-5">
-            <FileCheck />
-          </div>
-          <div className="flex-1 flex-col">
-            <h3 className="font-medium text-base">No need to switch app</h3>
-            <p className="text-sm text-gray-500 my-1">
-              Lorem ipsum dolor sit amet consectetur adipiscing elit. Pariatur minima sequi
-              recusandae, porro maiores officia.
-            </p>
-          </div>
-        </div>
+        <ListItem
+          title="Fill it digitally"
+          text="Lorem ipsum dolor sit amet consectetur adipiscing elit. Pariatur minima sequi
+              recusandae, porro maiores officia."
+          icon={<FileCheck />}
+          bgColor="bg-red-400"
+        />
+        <ListItem
+          title="Save it for other cases"
+          text="Lorem ipsum dolor sit amet consectetur adipiscing elit. Pariatur minima sequi
+              recusandae, porro maiores officia."
+          icon={<FileCheck />}
+          bgColor="bg-blue-400"
+        />
+        <ListItem
+          title="No need to switch app"
+          text="Lorem ipsum dolor sit amet consectetur adipiscing elit. Pariatur minima sequi
+              recusandae, porro maiores officia."
+          icon={<FileCheck />}
+          bgColor="bg-cyan-700"
+        />
       </div>
     </div>
   );
--- src/core/badges/Badge.js
+++ src/core/badges/Badge.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const Badge = ({ text }) => {
+const Badge = ({ text, ...props }) => {
   return (
-    <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-medium leading-4 border border-[#F59E0B] text-[#B65811] bg-yellow-50 rounded-full h-6">
-      {text}
-    </span>
+    <div {...props}>
+      <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-medium leading-4 border border-[#F59E0B] text-[#B65811] bg-yellow-50 rounded-full h-6">
+        {text}
+      </span>
+    </div>
   );
 };
 
